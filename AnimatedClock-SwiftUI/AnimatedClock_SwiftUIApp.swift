790e99398c1059837db0a38fab4eb93476d0823f
Jordan Christensen <jordan.c4922@gmail.com>
2020-10-23 16:26:27 -0600

    Initial Commit

---
+++ AnimatedClock-SwiftUI/AnimatedClock_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedClock_SwiftUIApp.swift
+//  AnimatedClock-SwiftUI
+//
+//  Created by Jordan Christensen on 10/23/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedClock_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
